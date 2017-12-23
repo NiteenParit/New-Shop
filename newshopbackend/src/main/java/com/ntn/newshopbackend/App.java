4b63a5cff7e8e82f7bdb69d07ae9246a4be693ef
Niteen Parit <niteenvps92@gmail.com>
2017-12-22 19:16:28 -0500

    My project setup completed successfully..

---
+++ newshopbackend/src/main/java/com/ntn/newshopbackend/App.java
@@ -0,0 +1,13 @@
+package com.ntn.newshopbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
